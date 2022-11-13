757763f3a0f8c4b79ef6361145900c428f3f15c1
invincibleinventor <invincibleinventor@gmail.com>
2022-11-13 22:41:46 +0530

    ok

--- src/routes/events/index.tsx
+++ src/routes/events/index.tsx
@@ -16,7 +16,7 @@ export default component$(() => {
     <div class="flex flex-col my-auto lg:pl-10  my-20 lg:py-0 lg:pt-0 ">
     <h1 class="font-bold text-white drop-shadow-xl font-poppins mb-2 lg:mb-[4px] lg:text-blue-50  text-4xl md:text-5xl lg:text-5xl mx-auto lg:mx-0 block">Verseny'22</h1>
     <h1 class=" font-poppins leading-loose font-medium  lg:font-normal lg:pl-1 sm:text-lg text-md mx-auto lg:mx-0 my-4 mt-2 md:mt-4 lg:my-0 text-white lg:text-blue-100  lg:text-left text-center lg:text-[20px] md:w-4/6 md:leading-9 px-9 md:px-0 lg:mt-3 lg:leading-8">Verseny'22 is the flagship event of Swarnotsav. It refers to an array of interschool events organized to engage and empower young pupils. <span class="hidden sm:inline-block xl:block"> Scroll down to view them.</span></h1>
-    <a href="/events" class="lg:mx-0 mx-6 bg-white shadow-2xl font-medium lg:font-semibold font-poppins text-blue-900 px-10 mt-3 mb-3 block lg:hidden lg:mt-0 transition-all ease-linear duration-100 hover:scale-105 py-3 rounded-md text-lg text-center"><span class="mx-auto">View Events</span></a>
+    <a href="/registration" class="lg:mx-0 mx-6 bg-white shadow-2xl font-medium lg:font-semibold font-poppins text-blue-900 px-10 mt-3 mb-3 block lg:hidden lg:mt-0 transition-all ease-linear duration-100 hover:scale-105 py-3 rounded-md text-lg text-center"><span class="mx-auto">Register Now</span></a>
 
     </div>
     </div>
