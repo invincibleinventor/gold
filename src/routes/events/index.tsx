491e394fc1242251cf16d196de7a92ff248daf76
invincibleinventor <invincibleinventor@gmail.com>
2022-11-14 10:19:24 +0530

    ok

--- src/routes/events/index.tsx
+++ src/routes/events/index.tsx
@@ -3,21 +3,21 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 import Card from '~/components/card/card';
 
-import img1 from "../images/1.jpg"
-import img2 from "../images/2.jpg"
-import img3 from "../images/3.jpg"
-import img4 from "../images/4.jpg"
-import img5 from "../images/5.jpg"
-import img6 from "../images/6.jpg"
-import img7 from "../images/7.jpg"
-import img8 from "../images/8.jpg"
-import img9 from "../images/9.jpg"
-import img10 from "../images/10.jpg"
-import img11 from "../images/11.jpg"
-import img12 from "../images/12.jpg"
-import img13 from "../images/13.jpg"
-import img14 from "../images/14.jpg"
-import img15 from "../images/15.jpg"
+import img1 from "1.jpg"
+import img2 from "2.jpg"
+import img3 from "3.jpg"
+import img4 from "4.jpg"
+import img5 from "5.jpg"
+import img6 from "6.jpg"
+import img7 from "7.jpg"
+import img8 from "8.jpg"
+import img9 from "9.jpg"
+import img10 from "10.jpg"
+import img11 from "11.jpg"
+import img12 from "12.jpg"
+import img13 from "13.jpg"
+import img14 from "14.jpg"
+import img15 from "15.jpg"
 
 
 export default component$(() => {
