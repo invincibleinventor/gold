197d05dc080a354c1c20b7d38081fc3c4a893ab5
InvincibleInventor <invincibleinventor@gmail.com>
2022-12-16 17:16:29 +0530

    ok

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,13 @@
 import { component$, } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-import Card from '~/components/card/card';
 
 export default component$(() => {
   
 
   return (
  <div class="flex my-auto mx-auto flex flex-col space-y-10">
-  
+
  </div>
   );
 });
